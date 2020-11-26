891b8d484b8bd79cea4eb2d3337f043c17d3e40d
jonn <jonn@netlight.com>
2020-11-26 16:28:41 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import VideoPage from './components/molecules/VideoPage/VideoPage'
 import Home from './Home'
