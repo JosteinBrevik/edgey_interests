9c79f4494a4cdfd7b482e2a4fc62becec599ec95
jonn <jonn@netlight.com>
2020-11-26 09:52:51 +0100

    terrafrom cleanup and ci variables

--- aws/iam.tf
+++ aws/iam.tf
@@ -20,18 +20,3 @@ data "aws_iam_policy_document" "website_policy" {
     }
   }
 }
-
-# data "aws_iam_policy_document" "website_policy" {
-#   statement {
-#     actions = [
-#       "s3:GetObject"
-#     ]
-#     principals {
-#       identifiers = ["*"]
-#       type = "AWS"
-#     }
-#     resources = [
-#       "arn:aws:s3:::${var.domain_name}/*"
-#     ]
-#   }
-# }
