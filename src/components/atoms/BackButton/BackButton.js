589029be625b53bc9cc2e4fe1f0078b6a8b794cc
jonn <jonn@netlight.com>
2020-11-27 12:01:48 +0100

    back button and css video page

--- src/components/atoms/BackButton/BackButton.js
+++ src/components/atoms/BackButton/BackButton.js
@@ -1,7 +1,13 @@
 import './BackButton.css';
+import BackArrowSVG from "../../../assets/back-arrow.svg"
+import {
+    Link
+  } from "react-router-dom";
 
-const BackButton = () =>(
-    <div></div>
+const BackButton = ({BackPage}) =>(
+    <Link className="BackButton" to={"/"+ BackPage}>
+        <img src={BackArrowSVG} alt="back arrow"/>
+    </Link>
 )
 
-export default VideoPage;
+export default BackButton;
--- src/components/molecules/VideoPage/VideoPage.js
+++ src/components/molecules/VideoPage/VideoPage.js
@@ -1,12 +1,16 @@
-import './VideoPage.css';
-import VideoPlayer from '../../atoms/VideoPlayer/VideoPlayer'
+import "./VideoPage.css";
+import VideoPlayer from "../../atoms/VideoPlayer/VideoPlayer";
+import BackButton from "../../atoms/BackButton/BackButton";
 
-const VideoPage = ({videoID}) =>{
+const VideoPage = ({ videoID }) => {
   return (
-      <body className="VideoPage-body">
-        <VideoPlayer videoID={videoID} />
-      </body>
+    <body className="VideoPage-body">
+      <div className="video-back-button">
+        <BackButton BackPage="" />
+      </div>
+      <VideoPlayer videoID={videoID} />
+    </body>
   );
-}
+};
 
 export default VideoPage;
