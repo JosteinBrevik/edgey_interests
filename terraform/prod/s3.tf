64fec16f5cf729567b66f7e60b907b77526fda23
jonn <jonn@netlight.com>
2020-12-01 13:58:51 +0100

    terrafrom refactoring into dev and prod, added prod terrraform files

---
+++ terraform/prod/s3.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "winterparty2020_bucket_prod" {
+  bucket = var.domain_name
+  acl = "private"
+  policy = data.aws_iam_policy_document.website_policy.json
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+
+  lifecycle {
+  create_before_destroy = true
+}
+}
