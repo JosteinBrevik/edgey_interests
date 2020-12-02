3341cee2d064f4989908e5dc6af2dd2ce4f3835c
jonn <jonn@netlight.com>
2020-12-02 14:54:02 +0100

    added snow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Routes from './Routes'
+import Snowfall from 'react-snowfall'
 
 const App = () =>{
   return (
+    <div>
     <Routes/>
+    <Snowfall snowflakeCount={50} color="#fffafa" />
+    </div>
   );
 }
 
