e4a72aa83ee315d7af4643ce522d9c02efd25721
Jostein Brevik <jostein.brevik@gmail.com>
2021-03-03 19:59:14 +0100

    Cleanup

---
+++ src/components/atoms/icons/background.js
@@ -0,0 +1,178 @@
+const Background = () => {
+  return (
+    <defs>
+      <pattern
+        id='pattern0'
+        patternContentUnits='objectBoundingBox'
+        width='1'
+        height='1'
+      >
+        <use
+          href='#image0'
+          transform='translate(0 -0.000422771) scale(0.00236407 0.00215699)'
+        />
+      </pattern>
+      <linearGradient
+        id='paint0_linear'
+        x1='4866'
+        y1='1482.2'
+        x2='5481.01'
+        y2='-2222.75'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stop-color='#0F172C' />
+        <stop offset='1' stop-color='#6B5A89' />
+      </linearGradient>
+      <linearGradient
+        id='paint1_linear'
+        x1='2319'
+        y1='1446.32'
+        x2='2304.5'
+        y2='1864.82'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stop-color='#D7E3F4' />
+        <stop offset='0.905451' stop-color='#8393D7' />
+      </linearGradient>
+      <linearGradient
+        id='paint2_linear'
+        x1='2551.8'
+        y1='1426.17'
+        x2='2498.5'
+        y2='1534'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stop-color='#A47D91' />
+        <stop offset='1' stop-color='#BFC6E7' />
+      </linearGradient>
+      <linearGradient
+        id='paint3_linear'
+        x1='3064.31'
+        y1='1286.49'
+        x2='3064.31'
+        y2='1620.94'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stop-color='#535786' />
+        <stop offset='1' stop-color='#535786' stop-opacity='0' />
+      </linearGradient>
+      <linearGradient
+        id='paint4_linear'
+        x1='46.6672'
+        y1='0'
+        x2='46.6672'
+        y2='334.448'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stop-color='#535786' />
+        <stop offset='1' stop-color='#535786' stop-opacity='0' />
+      </linearGradient>
+      <linearGradient
+        id='paint5_linear'
+        x1='3484.31'
+        y1='1286.49'
+        x2='3484.31'
+        y2='1743.44'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stop-color='#535786' />
+        <stop offset='1' stop-color='#535786' stop-opacity='0' />
+      </linearGradient>
+      <radialGradient
+        id='paint6_radial'
+        cx='0'
+        cy='0'
+        r='1'
+        gradientUnits='userSpaceOnUse'
+        gradientTransform='translate(3050.5 1201.88) rotate(105.255) scale(11.4018 10.5873)'
+      >
+        <stop stop-color='white' />
+        <stop offset='1' stop-color='white' stop-opacity='0' />
+      </radialGradient>
+      <radialGradient
+        id='paint7_radial'
+        cx='0'
+        cy='0'
+        r='1'
+        gradientUnits='userSpaceOnUse'
+        gradientTransform='translate(3244.5 1508.56) rotate(90) scale(79.5625 66.5)'
+      >
+        <stop stop-color='#FFACAC' />
+        <stop offset='1' stop-color='#010E53' stop-opacity='0' />
+      </radialGradient>
+      <radialGradient
+        id='paint8_radial'
+        cx='0'
+        cy='0'
+        r='1'
+        gradientUnits='userSpaceOnUse'
+        gradientTransform='translate(3239.58 1539.5) rotate(90) scale(66.5 55.5821)'
+      >
+        <stop stop-color='#FFACAC' />
+        <stop offset='1' stop-color='#A29C93' stop-opacity='0' />
+      </radialGradient>
+      <linearGradient
+        id='paint9_linear'
+        x1='-28.9095'
+        y1='-78.2082'
+        x2='106.707'
+        y2='152.695'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stop-color='white' />
+        <stop offset='1' stop-color='white' stop-opacity='0' />
+      </linearGradient>
+      <linearGradient
+        id='paint10_linear'
+        x1='-18.3837'
+        y1='-49.7328'
+        x2='67.855'
+        y2='97.099'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stop-color='white' />
+        <stop offset='1' stop-color='white' stop-opacity='0' />
+      </linearGradient>
+      <linearGradient
+        id='paint11_linear'
+        x1='-17.6483'
+        y1='-47.7436'
+        x2='65.1409'
+        y2='93.2153'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stop-color='white' />
+        <stop offset='1' stop-color='white' stop-opacity='0' />
+      </linearGradient>
+      <clipPath id='clip0'>
+        <rect width='4744' height='2778' fill='white' />
+      </clipPath>
+      <clipPath id='clip1'>
+        <rect
+          width='53'
+          height='34'
+          fill='white'
+          transform='translate(4004 2054)'
+        />
+      </clipPath>
+      <clipPath id='clip2'>
+        <rect
+          width='34'
+          height='21'
+          fill='white'
+          transform='translate(4123 2024)'
+        />
+      </clipPath>
+      <clipPath id='clip3'>
+        <rect
+          width='33'
+          height='20'
+          fill='white'
+          transform='translate(4060 1944)'
+        />
+      </clipPath>
+    </defs>
+  );
+};
+
+export default Background;
